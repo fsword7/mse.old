744090d03876b43df025da18f17611a4cbaaa88e
Timothy Stark <fsword7@verizon.net>
2017-02-19 20:56:24 -0500

    Implemented WinSock2 API interface for Windows.

--- src/emu/osd/main.cpp
+++ src/emu/osd/main.cpp
@@ -1,8 +1,21 @@
 
 #include "emu/core.hpp"
+#include "emu/osd/socket.h"
+
+#if __WIN32__
+static WSADATA wsaData;
+#endif
 
 int main(int argc, char **argv)
 {
+#if __WIN32__
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		printf("Winsock 2 API Error Code: %d", WSAGetLastError());
+		return 1;
+	}
+#endif
+
 	printf("Welcome to Multi-System Emulator System\n\n");
 
 	cliPrompt();
