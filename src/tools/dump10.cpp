d77bcc3f8d27122b43f4db840d266b7987459fcc
Timothy Stark <fsword7@verizon.net>
2017-02-20 16:51:21 -0500

    Implemented initial tools for PDP10-format files.

---
+++ src/tools/dump10.cpp
@@ -0,0 +1,16 @@
+/*
+ * dump10.cpp
+ *
+ *  Created on: Feb 20, 2017
+ *      Author: Timothy Stark
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		printf("Usage: dump10 [options] <file>\n");
+		return 1;
+	}
+}
