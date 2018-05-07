efe239540475268d21c59dadd2124300b94396fd
Timothy Stark <fsword007@gmail.com>
2018-05-07 14:57:24 -0400

    Created initial TELNET character codes for initialization and initializing table for new TELNET connections.

---
+++ src/emu/osd/telnet.cpp
@@ -0,0 +1,22 @@
+/*
+ * telnet.cpp
+ *
+ *  Created on: May 7, 2018
+ *      Author: Timothy Stark
+ */
+
+#include "emu/osd/telnet.h"
+
+// Telnet Code Initialization
+static char tlnCodes[] =
+{
+	TLN_IAC, TLN_WILL, TLN_LINEMODE,  // IAC WILL LINEMODE
+	TLN_IAC, TLN_WILL, TLN_SGA,       // IAC WILL SGA
+	TLN_IAC, TLN_WILL, TLN_ECHO,      // IAC WILL ECHO
+	TLN_IAC, TLN_WILL, TLN_BINARY,    // IAC WILL BINARY
+	TLN_IAC, TLN_DO,   TLN_BINARY     // IAC DO BINARY
+};
+
+
+
+
