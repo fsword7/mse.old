39eb78179a7ab6a6fcabb45c3ccc188654831c87
Timothy Stark <fsword007@gmail.com>
2019-01-06 23:27:39 -0500

    Added new base device to replace old Device class facility. Added diexec class for CPU devices to handle execution routines.

---
+++ src/emu/device.h
@@ -0,0 +1,13 @@
+/*
+ * device.h - Base Device Class facility
+ *
+ *  Created on: Jan 6, 2019
+ *      Author: Tim Stark
+ */
+
+#pragma once
+
+class device
+{
+
+};
---
+++ src/emu/diexec.h
@@ -0,0 +1,12 @@
+/*
+ * diexec.h - Device Interface - Execution
+ *
+ *  Created on: Jan 6, 2019
+ *      Author: Tim Stark
+ */
+
+#pragma once
+
+class diExecution {
+
+};
