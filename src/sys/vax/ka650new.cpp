fb350cc04ff988abd57c2438169e9965470063bf
Timothy Stark <fsword007@gmail.com>
2019-01-09 18:51:42 -0500

    Implemented new KA650 system with new system device to replace old KA650 devices.

---
+++ src/sys/vax/ka650new.cpp
@@ -0,0 +1,14 @@
+/*
+ * ka650new.cpp - KA650 system device
+ *
+ *  Created on: Jan 9, 2019
+ *      Author: Tim Stark
+ */
+
+#include "emu/emucore.h"
+#include "emu/devsys2.h"
+#include "sys/vax/ka650new.h"
+
+
+
+
---
+++ src/sys/vax/ka650new.h
@@ -0,0 +1,13 @@
+/*
+ * ka650new.h
+ *
+ *  Created on: Jan 9, 2019
+ *      Author: Tim Stark
+ */
+
+#pragma once
+
+class ka650_device : public system_device
+{
+
+};
