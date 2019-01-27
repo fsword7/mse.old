ad5015aa9f2a11a9f2e290d8f9efaeb4beefbb2e
Timothy Stark <fsword007@gmail.com>
2019-01-27 16:51:47 -0500

    Added required/optional devices for autoconfig.

--- src/emu/devauto.h
+++ src/emu/devauto.h
@@ -19,3 +19,20 @@ private:
 	tag_t			*tag;
 	bool			resolved;
 };
+
+template <class ObjectClass, bool required>
+class object_autodev_base : public devauto_base
+{
+protected:
+	object_autodev_base(device_t &base, tag_t *name)
+		: devauto_base(base, tag) { }
+
+private:
+	ObjectClass *target = nullptr;
+};
+
+template <class ObjectClass>
+using optional_device = object_autodev_base<ObjectClass, false>;
+
+template <class ObjectClass>
+using required_device = object_autodev_base<ObjectClass, true>;
