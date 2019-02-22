f65cc21b5cb94ed05c2b7a848a815dbc4b3a08d4
Timothy Stark <fsword007@gmail.com>
2019-02-21 22:54:30 -0500

    Commented old device creator line out (using new device type for device/system creator instead).

--- src/emu/driver.h
+++ src/emu/driver.h
@@ -13,14 +13,14 @@ class romEntry;
 class system_driver
 {
 public:
-	typedef device_t *(*creator_t)(const system_config &, tag_t *, device_t *, uint64_t);
+//	typedef device_t *(*creator_t)(const system_config &, tag_t *, device_t *, uint64_t);
 	typedef void (*system_creator)(system_config &, device_t &);
 	typedef void (*driver_init)(device_t &);
 
 	const char		*name;			// machine name
 	const char 		*parent;		// parent of machine name
 	const device_type_base	&type;
-	creator_t		creator;		// system device creator
+//	creator_t		creator;		// system device creator
 	system_creator	create;			// system create callback
 	driver_init		init;			// system initialize callback
 	romEntry		*rom;			// ROM entries for firmware
@@ -38,7 +38,6 @@ extern const system_driver SYSTEM_NAME(Name) =		\
 	#Name,											\
 	#Parent,										\
 	SYSTEM_TYPE(Name, Class),						\
-	device_t::create<Class>,						\
 	[] (system_config &config, device_t &owner) { static_cast<Class &>(owner).Create(config); }, \
 	[] (device_t &owner) { static_cast<Class &>(owner).Init(); },	\
 	ROM_NAME(Name),		\
