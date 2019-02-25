1b3ecad3c5f85e8a095c1ddce4b061cbead730d6
Timothy Stark <fsword007@gmail.com>
2019-02-25 13:22:43 -0500

    Now compiling with zlib libraries. Implemented new zlib data processing.

---
+++ src/lib/util/fileio.cpp
@@ -0,0 +1,17 @@
+/*
+ * fileio.cpp
+ *
+ *  Created on: Feb 25, 2019
+ *      Author: fswor
+ */
+
+#include <iostream>
+
+#include <cstdio>
+#include <cstdint>
+#include <cassert>
+
+#include <zlib.h>
+
+#include "lib/util/fileio.h"
+
