2cf57817d4c2c41ffa504c92335e0296a4183a2d
Timothy Stark <fsword007@gmail.com>
2019-02-25 15:34:55 -0500

    Now includes zip/7z headers.

--- src/lib/util/unzip.h
+++ src/lib/util/unzip.h
@@ -6,3 +6,19 @@
  */
 
 #pragma once
+
+// LZMA library files
+#include "lzma/C/7z.h"
+#include "lzma/C/7zAlloc.h"
+#include "lzma/C/7zCrc.h"
+#include "lzma/C/7zTypes.h"
+#include "lzma/C/LzmaDec.h"
+
+class archive_file
+{
+public:
+
+//	static void open_zip(const std::string fileName);
+//	static void open_7z(const std::string fileName);
+
+};
