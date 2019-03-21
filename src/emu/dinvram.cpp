b84987b903475b82e54e5435ce836b8d76a9815b
Timothy Stark <fsword007@gmail.com>
2019-03-21 13:11:13 -0400

    Added NVRAM device interface base class.

---
+++ src/emu/dinvram.cpp
@@ -0,0 +1,19 @@
+/*
+ * dinvram.cpp - NVRAM device interface
+ *
+ *  Created on: Mar 21, 2019
+ *      Author: Tim Stark
+ */
+
+#include "emu/emucore.h"
+#include "emu/dinvram.h"
+
+di_nvram::di_nvram(system_config &config, device_t &device)
+: device_interface(device, "nvram")
+{
+}
+
+di_nvram::~di_nvram()
+{
+}
+
---
+++ src/emu/dinvram.h
@@ -0,0 +1,24 @@
+/*
+ * dinvram.h - NVRAM device interface
+ *
+ *  Created on: Mar 21, 2019
+ *      Author: Tim Stark
+ */
+
+#pragma once
+
+class di_nvram : public device_interface
+{
+public:
+	di_nvram(system_config &config, device_t &device);
+	virtual ~di_nvram();
+
+	void reset() { nvramDefault(); }
+	void load(emuFile &file) { nvramRead(file); }
+	void save(emuFIle &file) { nvramWrite(file); }
+
+protected:
+	virtual void nvramDefault() = 0;
+	virtual void nvramRead(emuFile &file) = 0;
+	virtual void nvramWrite(emuFile &file) = 0;
+};
