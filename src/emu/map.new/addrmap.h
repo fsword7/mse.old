d9654957ea2a7757051a0e0c971374fd29613668
Timothy Stark <fsword007@gmail.com>
2019-03-26 10:47:35 -0400

    Added some config calls to new mapAddressEntry class. Added memory space lists to new mapMemoryManager class.

--- src/emu/map.new/addrmap.h
+++ src/emu/map.new/addrmap.h
@@ -7,6 +7,8 @@
 
 #pragma once
 
+class mapAddress;
+
 enum mapAddressType {
 	mapNone = 0,
 	mapRAM,			// RAM type
@@ -29,10 +31,61 @@ struct mapHandler
 
 class mapAddressEntry
 {
+public:
+	mapAddressEntry(device_t &device, mapAddress &map, offs_t start, offs_t end);
+
+	// MAP/ROM space mapping
+	mapAddressEntry &ram()		{ read.type = mapRAM, write.type = mapRAM; return *this; }
+	mapAddressEntry &rom()		{ read.type = mapRAM, write.type = mapNop; return *this; }
+	mapAddressEntry &ronly()	{ read.type = mapRAM; return *this; }
+	mapAddressEntry &wonly()	{ write.type = mapRAM; return *this; }
+
+	// NOP space mapping
+	mapAddressEntry &noprw()	{ read.type = mapNop, write.type = mapNop; return *this; }
+	mapAddressEntry &nopr()		{ read.type = mapNop; return *this; }
+	mapAddressEntry &nopw()		{ write.type = mapNop; return *this; }
+
+	mapAddressEntry &mask(offs_t bits)		{ addrMask = bits; return *this; }
+	mapAddressEntry &mirror(offs_t bits)	{ addrMirror = bits; return *this; }
+	mapAddressEntry &select(offs_t bits)	{ addrSelect = bits; return *this; }
+
+	mapAddressEntry &region(tag_t *name, offs_t offset = 0)
+		{ tagRegion = name; offRegion = offset; return *this; }
+	mapAddressEntry &share(tag_t *name)		{ tagShare = name; return *this; }
+
+
+	mapAddress	&map;			// Reference to map manager
+	device_t	&device;		// Reference to device
+
+	offs_t		addrStart;		// Start address
+	offs_t		addrEnd;		// End address
+	offs_t		addrMask;		// Mask address bits
+	offs_t		addrMirror;		// Mirror address bits
+	offs_t		addrSelect;		// Select address bits
+
+	mapHandler	read, write;	// Read/write entry handler
+	offs_t		offRegion;		// Region offset from base
+	tag_t		*tagRegion;		// Region tag name
+	tag_t		*tagShare;		// Share tag name
+
+	void		*memory;		// Assigned memory space
+
 };
 
 class mapAddress
 {
+	mapAddress(device_t &device, int space);
+	mapAddress(device_t &device, mapAddressEntry &entry);
+	~mapAddress();
+
+	mapAddressEntry operator ()(offs_t start, offs_t end);
+
+	offs_t	gmask;			// Global addressing mask
+	int		unmapValue;		// Unmapped value
+
 private:
+	device_t	&device;
+	int			addrSpace;	// Address space type
+
 	std::vector <mapAddressEntry> list;
 };
--- src/emu/map.new/manager.h
+++ src/emu/map.new/manager.h
@@ -13,6 +13,18 @@ public:
 	mapMemoryManager();
 	~mapMemoryManager();
 
+	// Getters
+	std::vector<mapMemoryBlock *> &blocks() const		{ return blockList; }
+	std::vector<mapMemoryBank *> &banks() const			{ return bankList; }
+	std::vector<mapMemoryRegion *> &regions() const		{ return regionList; }
+	std::vector<mapMemoryShare *> &shares() const		{ return shareList; }
+
 private:
 	machine &system;
+
+	// Memory spaces
+	std::vector<mapMemoryBlock *> blockList;
+	std::vector<mapMemoryBank *> bankList;
+	std::vector<mapMemoryRegion *> regionList;
+	std::vector<mapMemoryShare *> shareList;
 };
